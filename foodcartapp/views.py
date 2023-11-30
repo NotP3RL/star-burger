adb3c8f335a1e1884ab5ab9c47c80cba21311593
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2023-11-30 21:52:55 +0800

    Extend data validation for def register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -2,7 +2,8 @@ import datetime
 
 from django.http import JsonResponse
 from django.templatetags.static import static
-import json
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from phonenumber_field.validators import validate_international_phonenumber
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -62,12 +63,38 @@ def product_list_api(request):
     })
 
 
+def check_str_keys(order_info):
+    not_presented_str_keys = []
+    empty_str_keys = []
+    not_str_keys = []
+    supposed_str_keys = ['firstname', 'lastname', 'phonenumber', 'address']
+    for supposed_str_key in supposed_str_keys:
+        if not supposed_str_key in order_info:
+            not_presented_str_keys.append(supposed_str_key)
+        elif not order_info[supposed_str_key]:
+            empty_str_keys.append(supposed_str_key)
+        elif not isinstance(order_info[supposed_str_key], str):
+            not_str_keys.append(supposed_str_key)
+    return not_presented_str_keys, empty_str_keys, not_str_keys
+
+
 @api_view(['POST'])
 def register_order(request):
     try:
         order_info = request.data
-        if not 'products' in order_info:
-            content = {'error': 'products key no present'}
+        checked_str_keys = check_str_keys(order_info)
+
+        if checked_str_keys[0]:
+            content = {'error': f'{", ".join(checked_str_keys[0])} keys not present'}
+            return Response(content, status=status.HTTP_200_OK)
+        elif checked_str_keys[1]:
+            content = {'error': f'{", ".join(checked_str_keys[1])} keys cannot be null'}
+            return Response(content, status=status.HTTP_200_OK)
+        elif checked_str_keys[2]:
+            content = {'error': f'{", ".join(checked_str_keys[2])} keys are not string'}
+            return Response(content, status=status.HTTP_200_OK)
+        elif not 'products' in order_info:
+            content = {'error': 'products key not present'}
             return Response(content, status=status.HTTP_200_OK)
         elif not order_info['products']:
             content = {'error': 'products key cannot be null'}
@@ -76,6 +103,7 @@ def register_order(request):
             content = {'error': 'products key is not list'}
             return Response(content, status=status.HTTP_200_OK)
         else:
+            validate_international_phonenumber(order_info['phonenumber'])
             offset = datetime.timedelta(hours=-8)
             timezone = datetime.timezone(offset, name='UTC')
             order_time = datetime.datetime.now(tz=timezone)
@@ -100,3 +128,10 @@ def register_order(request):
         return JsonResponse({
             'error': 'ValueError',
         })
+    except ObjectDoesNotExist:
+        content = {'error': 'products id does not exist'}
+        return Response(content, status=status.HTTP_200_OK)
+    except ValidationError:
+        content = {'error': 'phonenumber is not correct'}
+        return Response(content, status=status.HTTP_200_OK)
+
